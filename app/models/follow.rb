37d18ba94487934e0642bdbd3174992328745e22
Lucas Medeiros Leite <lucastoc@gmail.com>
2016-06-04 08:57:43 -0300

    follower.rb enhacement

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -6,4 +6,21 @@ class Follow
   field :followee_id, type: String
   field :follower_class, type: String
   field :follower_id, type: String
+
+  def self.store_by!(followee:, follower:)
+    find_or_create_by!({
+      followee_class: followee.class.to_s,
+      followee_id: followee.id,
+      follower_class: follower.class.to_s,
+      follower_id: follower.id })
+  end
+
+  def self.destroy_by!(followee:, follower:)
+    find_by({
+      followee_class: followee.class.to_s,
+      followee_id: followee.id,
+      follower_class: follower.class.to_s,
+      follower_id: follower.id })
+    .try(:destroy)
+  end
 end
--- lib/mongoid_followit/follower.rb
+++ lib/mongoid_followit/follower.rb
@@ -2,14 +2,37 @@ module Mongoid
   module Followit
     module Follower
       def self.included(base)
+        generate_callbacks(base)
+        add_callbacks(base)
+      end
+
+      def follow(*followees)
+        perform_followable_action(:follow, followees)
+      end
+
+      def unfollow(*followees)
+        perform_followable_action(:unfollow, followees)
+      end
+
+      def followees(criteria: false)
+        grouped = Follow.where(follower_class: self.class, follower_id: id)
+                        .group_by(&:followee_class)
+        criteria ? followees_as_criteria(grouped) : followees_as_array(grouped)
+      end
+
+      private_class_method
+
+      def self.add_callbacks(base)
         base.class_eval do
           include ActiveSupport::Callbacks
           define_callbacks :follow, :unfollow
           before_destroy :destroy_follow_data
         end
+      end
 
-        ['before', 'after'].freeze.each do |callback|
-          ['follow', 'unfollow'].freeze.each do |action|
+      def self.generate_callbacks(base)
+        %w(before after).each do |callback|
+          %w(follow unfollow).each do |action|
             base.define_singleton_method("#{callback}_#{action}") do |*ar, &blo|
               set_callback(action.to_sym, callback.to_sym, *ar, &blo)
             end
@@ -17,64 +40,38 @@ module Mongoid
         end
       end
 
-      def follow(*followees)
-        warn_non_unfollowee_existence(followees)
-
-        run_callbacks :follow do
-          followees.each do |followee|
-              Follow.find_or_create_by!({
-                followee_class: followee.class.to_s,
-                followee_id: followee.id,
-                follower_class: self.class.to_s,
-                follower_id: self.id
-              })
-          end
-        end
-      end
-
-      def unfollow(*followees)
-        warn_non_unfollowee_existence(followees)
-
-        run_callbacks :unfollow do
-          followees.each do |followee|
-            Follow.find_by({
-              followee_class: followee.class.to_s,
-              followee_id: followee.id,
-              follower_class: self.class.to_s,
-              follower_id: self.id
-            }).try(:destroy)
-          end
-        end
-      end
-
-      def followees(criteria: false)
-        grouped = Follow.where({
-          follower_class: self.class,
-          follower_id: self.id
-        }).group_by { |f| f.followee_class }
-
-        criteria ? followees_as_criteria(grouped) : followees_as_array(grouped)
-      end
-
       private
 
+      def perform_followable_action(action, followables)
+        warn_non_unfollowee_existence(followables)
+        run_callbacks(action) do
+          followables.each do |followable|
+            if :follow == action
+              Follow.store_by!(followee: followable, follower: self)
+            else
+              Follow.destroy_by!(followee: followable, follower: self)
+            end
+          end
+        end
+      end
+
       def warn_non_unfollowee_existence(followees)
-        unless followees.any? { |f| f.kind_of?(Mongoid::Followit::Followee) }
+        unless followees.any? { |f| f.is_a?(Mongoid::Followit::Followee) }
           raise 'Object(s) must include a Mongoid::Followit::Followee'
         end
       end
 
       def destroy_follow_data
-        Follow.or({ followee_class: self.class.to_s, followee_id: self.id  },
-                  { follower_class: self.class.to_s, follower_id: self.id  })
-              .destroy_all
+        followee_params = { followee_class: self.class.to_s, followee_id: id  }
+        follower_params = { follower_class: self.class.to_s, follower_id: id  }
+        Follow.or(followee_params, follower_params).destroy_all
       end
 
       def followees_as_criteria(grouped)
         return Follow.none if grouped.empty?
         raise 'HasTwoFolloweeTypesError' if grouped.length > 1
         followee_class = grouped.keys.first
-        followee_ids = grouped[followee_class].map { |f| f.followee_id }
+        followee_ids = grouped[followee_class].map(&:followee_id)
         followee_class.constantize.in(id: followee_ids)
       end
 
